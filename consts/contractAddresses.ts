30b6953653733fe3d2821eabe51c978344e1a52a
Slimpennies <128172825+Slimpennies@users.noreply.github.com>
2023-04-10 22:45:23 -0400

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x4aa00C07748feC0d78b0cA0c7918eB64B2251187";
+export const tokenContractAddress =
+  "0x8D3B5877233124793c018eb1D8A5734d897f103B";
+export const stakingContractAddress =
+  "0x460bA12ad5F29cD4cd641B65436029262Aa5aAb8";
